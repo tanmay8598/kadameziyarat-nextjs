ac11b7cd880ec4c7158216555e7cb2aca9df90c6
TANMAY SRIVASTAVA <34513878+tanmay8598@users.noreply.github.com>
2025-08-29 12:56:09 +0530

    adsense script added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,28 @@
-import ClientLayout from './client-layout';
+import ClientLayout from "./client-layout";
+import Script from "next/script";
 
 export const metadata = {
-    title: "Kadam-e-Ziyarat | Ground and group packages to Iran, Iraq & Umrah",
-    description: "Looking for a trusted Ziyarat tour? Kadam-e-Ziyarat offers customized group & ground packages for Iran, Iraq & Umrah. Book now!",
+  title: "Kadam-e-Ziyarat | Ground and group packages to Iran, Iraq & Umrah",
+  description:
+    "Looking for a trusted Ziyarat tour? Kadam-e-Ziyarat offers customized group & ground packages for Iran, Iraq & Umrah. Book now!",
 };
 
 export default function RootLayout({ children }) {
-    return <ClientLayout>{children}</ClientLayout>;
+  return (
+    <html lang="en">
+      <head>
+        {/* Google AdSense */}
+        <Script
+          id="adsense-script"
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9723649193676693"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body>
+        <ClientLayout>{children}</ClientLayout>
+      </body>
+    </html>
+  );
 }
